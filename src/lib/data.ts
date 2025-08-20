b9c597d82bce410014c21e13511162252e9af843
Laura Webb <laura.pe.webb@gmail.com>
2025-08-20 08:51:31 +0000

    Control group in alphabetical order, will update for profiles

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,8 +2,8 @@
 import type { ClanMember } from "@/lib/types";
 
 export const clanMembers: ClanMember[] = [
-  { id: '1', name: 'James', avatar: 'https://placehold.co/40x40.png' },
-  { id: '2', name: 'Brendan', avatar: 'https://placehold.co/40x40.png' },
   { id: '3', name: 'Bobby', avatar: 'https://placehold.co/40x40.png' },
+  { id: '2', name: 'Brendan', avatar: 'https://placehold.co/40x40.png' },
+  { id: '1', name: 'James', avatar: 'https://placehold.co/40x40.png' },
   { id: '4', name: 'Seb', avatar: 'https://placehold.co/40x40.png' },
 ];
